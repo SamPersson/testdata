571b04eb90305e26d7100604444362eb53917e0a
Sam Persson <samiljan@gmail.com>
2016-09-14 11:02:03 +0200

    Added AddNewCustomer.aspx SE support

--- visma.js
+++ visma.js
@@ -1,8 +1,8 @@
 (function() {
+let randint = m => Math.floor(Math.random() * m);
+let pad = ((s, l) => new Array(l + 1 - s.length).join("0") + s);
 
 function CheckoutSE() {
-	let pad = ((s, l) => new Array(l + 1 - s.length).join("0") + s);
-	let randint = m => Math.floor(Math.random() * m);
 	let isValidRegNo = no => no.split("").filter(c => c !== "-").map(c => c - "0").map((c, i) => ((i + 1) % 2 + 1) * c).map(s => (s % 10) + Math.floor(s / 10)).reduce((a, b) => a + b) % 10 === 0;
 	let reg;
 	do {
@@ -19,8 +19,6 @@ function CheckoutSE() {
 }
 
 function CheckoutNO() {
-	let pad = ((s, l) => new Array(l + 1 - s.length).join("0") + s);
-	let randint = m => Math.floor(Math.random() * m);
 	let ctrl = [3, 2, 7, 6, 5, 4, 3, 2, 1];
 	let isValidRegNo = no => no.split("").filter(c => c !== "-").map(c => c - "0").map((c, i) => ctrl[i] * c).reduce((a, b) => a + b) % 11 === 0;
 	let reg;
@@ -37,10 +35,27 @@ function CheckoutNO() {
 	document.getElementById("AcceptTermsOfService").checked = true;
 }
 
+function NewVONCustomer() {
+	var customerNumber = document.getElementById("maincontentholder_CustomerNoTextBox").value;
+	if(!customerNumber.match("\d+")) {
+	  document.getElementById("maincontentholder_CustomerNoTextBox").value = customerNumber = randint(100000000);
+	}
+	document.getElementById("maincontentholder_CustomerNameTextBox").value = "s" + customerNumber;
+	document.getElementById("maincontentholder_NewOrgNoTextBox").value = "555555-5555";
+	document.getElementById("maincontentholder_NewInvoiceAddress1TextBox").value = "A";
+	document.getElementById("maincontentholder_NewInvoicePostalCodeTextBox").value = "22222";
+	document.getElementById("maincontentholder_NewInvoiceCityTextBox").value = "Lund";
+	document.getElementById("maincontentholder_EmailTextBox").value = "s" + customerNumber + "@vline.spcs.se";
+	document.getElementById("maincontentholder_FirstNameTextBox").value = "Sam";
+	document.getElementById("maincontentholder_LastNameTextBox").value = customerNumber;
+}
+
 if(window.location.href.match(/\/sv\-SE\/checkout\/buy\b/)) {
   CheckoutSE();
 } else if(window.location.href.match(/\/nb\-NO\/checkout\/buy\b/)) {
   CheckoutNO();
+} else if(window.location.href.match(/\/administration\/Internal\/AddNewCustomer\.aspx\b/)) {
+  NewVONCustomer();
 }
 	
 })();
