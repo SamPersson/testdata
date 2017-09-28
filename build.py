fcd9a232fdca70f9cc547a0ea02c2cc4d6b80307
Sam Persson <sam.persson@adhoc.se>
2017-09-28 12:39:18 +0200

    Add vscode build tasks and pack chrome automatically

--- build.py
+++ build.py
@@ -29,7 +29,8 @@ def create_addon(files, browser):
     output_dir = os.path.join(output_folder, 'testdata-%s' % (browser))
     
     if os.path.isdir(output_dir):
-        shutil.rmtree(output_dir)
+        os.rename(output_dir, output_dir+"_temp")
+        shutil.rmtree(output_dir+"_temp")
         
     os.mkdir(output_dir)       
 
@@ -52,11 +53,14 @@ def create_addon(files, browser):
             with open(of, "w") as outF:
                 outF.write(json.dumps(manifest, indent=2)) 
         else:
-            shutil.copy(f, of);
+            shutil.copy(f, of)
 
-    if browser == 'opera':
+    if browser == "chrome":
+        shutil.make_archive(output_dir, "zip", output_dir)
+
+    #if browser == 'opera':
         #Create .nex
-        os.system('./nex-build.sh %s %s %s' % (output_file, output_file.replace('.zip', '.nex'), cert))
+    #    os.system('./nex-build.sh %s %s %s' % (output_file, output_file.replace('.zip', '.nex'), cert))
 
 
 
@@ -67,7 +71,7 @@ if __name__ == '__main__':
 
     files = get_files_to_zip()
     
-    print('******* REDIRECTOR BUILD SCRIPT *******')
+    print('******* EXTENSION BUILD SCRIPT *******')
     print('')
 
     create_addon(files, 'chrome')
